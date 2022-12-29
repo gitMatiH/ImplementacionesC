20bf562a3510a24196064b87a6888422b5d1d286
Matías Hillmann <mailmatiash@gmail.com>
2022-12-29 17:40:19 -0300

    cosas

--- Implementaciones/colaEstaticaCircular.h
+++ Implementaciones/colaEstaticaCircular.h
@@ -9,30 +9,86 @@ typedef int tDato;
 
 typedef struct Cola
 {
-    tDato elem[CMAX-1];
+    tDato elem[CMAX];
     int inicio;
     int fin;
 }tCola;
 
+/*con conocimiento de longitudArr*/
+float porcentajeOcupacion(tCola* q, int longitudArr) {
+
+/**
+*   Precondicion: longitudArr (sino no se puede). Se saca al crear la estructura
+*   Poscondicion: devuelve porcentaje de ocupacion
+  */
+    float porcentaje;
+
+    if ( (*q).inicio < (*q).fin ) {
+        float aux = ((*q).fin - (*q).inicio + 1) * 100;
+        porcentaje = aux/longitudArr;
+        return porcentaje;
+    }
+
+    if ( (*q).inicio > (*q).fin ) {
+        float aux = longitudArr - ((*q).inicio + 1) + (*q).fin;
+        porcentaje = aux / longitudArr;
+        return porcentaje;
+    }
+    
+    if ( (*q).inicio == (*q).fin ) {
+        porcentaje = 100;
+        return porcentaje;
+    }
+
+}
+
+/*sin conocimiento de longitudArr*/
+
 void cCrear(tCola* q)
 {
+
+/**
+*   Precondicion: que se le pase una cola vacia
+*   Poscondicion: devuelve a la cola inicializada
+  */
+
     q->inicio = -1;
     q->fin = -1;
 }
 
 int cVacia(tCola* q)
 {
+
+/**
+*   Precondicion: no tiene
+*   Poscondicion: devuelve 1 si esta vacia, 0 si no esta vacia
+ */
+
     return q->inicio == q->fin;
 }
 
 int cLlena(tCola* q)
 
+/**
+*   Precondicion: no tiene
+*   Poscondicion: devuelve 1 si esta llena, 0 si no esta llena
+ */
+
 {
-    return ((q->inicio == 0 && q->fin == CMAX-1) || ((q->fin+1) == q->inicio));// asi no tiene sentido, q->(inicio+1), deberia ser q->inicio+1
+    return ( (q->inicio == 0 && q->fin == CMAX-1) ||
+           ( (q->fin)+1 == q->inicio) );
 }
+
+
 
 void cPoner(tCola* q, tDato x)
 {
+
+/**
+*   Precondicion: no cLlena()
+*   Poscondicion: pone el elemento x de tipo'' en el final de la cola
+ */
+
     if (q->fin == CMAX-1)
     {
         q->fin = 0;
@@ -44,7 +100,7 @@ void cPoner(tCola* q, tDato x)
     q->elem[q->fin] = x;
 }
 
-void cSacar(tCola* q, tDato* x) //ojo ahora tDato va por ref
+void cSacar(tCola* q, tDato* x)
 {
     if (q->inicio == CMAX-1)
     {
--- testCEC.c
+++ testCEC.c
@@ -5,11 +5,62 @@
 #include "Implementaciones/colaEstaticaCircular.h"
 
 
-void main{
+void estadoCola(int a) {
+	printf("\n");
+	
+	if (a == 0) {
+		printf("la cola no esta vacia");
+	}
+	else {
+		printf("la cola esta vacia");
+	}
+}
+
+void main() {
 
 	tCola cola1;
 	cCrear(&cola1);
 	bool sal;
-	sal = cVacia(cola1);
+	sal = cVacia(&cola1);
 	printf("%d", sal);
+	printf("\n");
+	int a = 7;
+	cPoner(&cola1, a);
+	sal = cVacia(&cola1);
+	printf("%d", sal);
+	printf("\n");
+	int b = 8;
+	cPoner(&cola1, b);
+	int c;
+	cSacar(&cola1, &c);
+	printf("\n");
+	printf("%d", c);
+	cSacar(&cola1, &c);
+	printf("\n");
+	printf("%d", c);
+
+	sal = cVacia(&cola1);
+	estadoCola(sal);
+	
+
+	b = 8;
+	cPoner(&cola1, b);
+	printf("\n");
+	sal = cVacia(&cola1);
+	estadoCola(sal);
+
+	printf("\n");
+	float porcentaje;
+	porcentaje = porcentajeOcupacion(&cola1, CMAX);
+	printf("%f", porcentaje);
+
+
+	while (cVacia(&cola1) == 0) {
+		cSacar(&cola1, &a);
+	}
+
+	sal = cVacia(&cola1);
+	estadoCola(sal);
+
+
 }
